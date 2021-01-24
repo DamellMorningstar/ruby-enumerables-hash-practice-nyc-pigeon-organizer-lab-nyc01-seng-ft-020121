d9852d8297febdf264d65c3b37b4964f63a4e073
Da-mell Faines <damellfaines8@gmail.com>
2021-01-24 17:36:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {
+    "Theo" => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Subway"]
+  }
+  }
 end
